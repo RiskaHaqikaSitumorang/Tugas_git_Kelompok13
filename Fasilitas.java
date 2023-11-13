f106d4cfc5f865772ea9db9bf04a3411387e1b6b
berlianiutami <132455356+berlianiutami@users.noreply.github.com>
2023-11-13 15:34:48 +0700

    Update Fasilitas.java

--- Fasilitas.java
+++ Fasilitas.java
@@ -13,3 +13,14 @@ abstract class Fasilitas {
         this.nama = nama;
         this.harga = harga;
     }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public abstract void tampilkanDetail();
+}
