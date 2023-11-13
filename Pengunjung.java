7a0690f5b08872222b1dbb4447454cfe44e06f52
berlianiutami <132455356+berlianiutami@users.noreply.github.com>
2023-11-13 16:33:43 +0700

    Create Pengunjung.java

---
+++ Pengunjung.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Pengunjung here.
+ *
+ * @author (Riska Haqika Situmorang & Berliani Utami)
+ * @version (11/13/2023)
+ */
+public class Pengunjung {
+    private String nama;
+    private int usia;
+
+    public Pengunjung(String nama, int usia) {
+        this.nama = nama;
+        this.usia = usia;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getUsia() {
+        return usia;
+    }
+}
