8ffefc8db84764d3d9e0f0a668d78513acfc3479
berlianiutami <132455356+berlianiutami@users.noreply.github.com>
2023-11-13 22:22:39 +0700

    Update Pemesanan.java

--- Pemesanan.java
+++ Pemesanan.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class Pemesanan here.
  *
- * @author (Riska Haqika Situmorang & Berliani Utami)
+ * @author (Berliani Utami & Riska Hakiqa Situmorang)
  * @version (13/11/2023)
  */
 import java.util.ArrayList;
